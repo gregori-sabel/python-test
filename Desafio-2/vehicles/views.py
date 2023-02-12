bcce59cf2335406699f87a5332fa6399dcfe8cab
Grégori Sabel <seriosabel57@gmail.com>
2023-02-11 22:29:40 -0300

    alot of things

--- Desafio-2/vehicles/views.py
+++ Desafio-2/vehicles/views.py
@@ -1,9 +1,32 @@
+from django.http import Http404
 from django.shortcuts import render
 
 # Create your views here.
 
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question
+from django.shortcuts import get_object_or_404
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+  latest_question_list = Question.objects.order_by('-pub_date')[:5]
+  context = {'latest_question_list': latest_question_list}
+  return render(request, 'vehicles/index.html', context)
+
+def detail(request, question_id):
+  # try:
+  #   question = Question.objects.get(pk=question_id)
+  # except:
+  #   raise Http404("Question does not exists")
+
+  question = get_object_or_404(Question, pk=question_id)
+  
+  return render(request, 'vehicles/detail.html', {'question': question})
+
+def results(request, question_id):
+  response =  "You're looking at the results of question %s."
+  return HttpResponse( response % question_id)
+
+def vote(request, question_id):
+  return HttpResponse("You're voting on question %s." % question_id)
